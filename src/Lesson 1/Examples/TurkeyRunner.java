0f6901353016d612df387e88d6303de275ae9110
Vongphak <vongphak1007@gmail.com>
2018-09-05 14:58:25 +0000

    Add Lesson 1

---
+++ src/Lesson 1/Examples/TurkeyRunner.java
@@ -0,0 +1,39 @@
+//(c) A+ Computer Science
+// www.apluscompsci.com
+
+//methods example 2 and 3
+
+import static java.lang.System.*;
+
+public class TurkeyRunner
+{
+   public static void main(String[] args)
+   {
+		System.out.println( "Test 1" );	   	
+      Turkey bird = new Turkey();
+      bird.speak();
+      bird.sayName();
+      bird.speak();
+      bird.sayName();
+      bird.speak();   
+
+		System.out.println( "\n\nTest 2" );		
+      Turkey it = new Turkey();
+      it.sayName();
+      it.sayName();    
+      	
+		System.out.println( "\n\nTest 3" );		
+      //make more Turkeys and 
+      //call the old methods and 
+      //the new method you created   
+      
+      Turkey jack = new Turkey();
+      jack.speak();
+      jack.speak();
+      jack.speak();
+      jack.sayName();
+      out.println(jack.sayAge(5));
+      jack.sayName();
+      jack.speak();          	 	        
+   }
+}
